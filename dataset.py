fce00efe7efe9b4df59ef54789f4639227aa6dc9
Aleksandar-Mladenoski <164692560+Aleksandar-Mladenoski@users.noreply.github.com>
2024-11-02 15:03:30 +0100

    Simple Dataset to access the
    Created FSDataset in order to loop over all the laps for each Grand Prix weekend, and provide the race results in the same way.

--- dataset.py
+++ dataset.py
@@ -1,31 +1,22 @@
 import pandas as pd
 import matplotlib
 import torch
-import torch.utils.data.dataset as Dataset
+from torch.utils.data import Dataset
 import numpy as np
-
-# df_clean = pd.read_csv(r'F1Data\f1_dropped_na.csv')
-# print(df_clean.head)
-# print(f"NA Values in df: {df_clean.isnan().values.sum()}")
-
-lap_data = pd.read_csv(r'F1Data\f1_dropped_na.csv')
-print(lap_data['GP'].count())
-years = np.sort(lap_data['Year'].unique())
-print(years)
-
 
 class FSDataset(Dataset):
     def __init__(self, lap_data_dir, final_results_dir):
         self.lap_data = pd.read_csv(lap_data_dir)
-        self.final_results_dir = pd.read_csv(final_results_dir)
+        self.final_results = pd.read_csv(final_results_dir)
+        self.samples_per_year = dict()
+        self.years = np.sort(self.lap_data['Year'].unique())
+        for year in self.years:
+            self.samples_per_year.update({year: self.lap_data[self.lap_data['Year'] == year]['GP'].nunique()})
 
     def __len__(self):
         samples = 0
-        self.samples_per_year = dict()
-        self.years = np.sort(self.lap_data['Year'].unique())
-        for year in years:
-            self.samples_per_year.update({year: self.lap_data[lap_data['Year'] == year]['GP'].nunique()})
-            samples += self.lap_data[lap_data['Year'] == year]['GP'].nunique()
+        for year in self.years:
+            samples += self.lap_data[self.lap_data['Year'] == year]['GP'].nunique()
         return samples
 
     def __getitem__(self, idx):
@@ -38,4 +29,15 @@ class FSDataset(Dataset):
                 break
             cum_sample += self.samples_per_year[self.years[i+1]]
 
-        laps = self.lap_data[(lap_data['Year'] == idx_year) & (lap_data['GP_num'] == idx_within_year)]
+        laps = self.lap_data[(self.lap_data['Year'] == idx_year) & (self.lap_data['GP_num'] == idx_within_year)]
+        result = self.final_results[(self.final_results['Year'] == idx_year) & (self.final_results['GP_num'] == idx_within_year)]
+
+        return laps, result
+
+
+
+dataset = FSDataset(r'F1Data\f1_dropped_na.csv', r'F1Data\f1_final_result_data.csv')
+
+for sample in dataset:
+    print(sample[0])
+    print(sample[1])
